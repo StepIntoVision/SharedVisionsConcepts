a322605ca3e3ecc03d2d59408383e6d433d72115
Joseph Simpson <joe@radicalappdev.com>
2026-02-10 15:57:23 -0500

    Adding an ornament card for notes

--- SharedVisionsConcepts01/Concepts/Concept001.swift
+++ SharedVisionsConcepts01/Concepts/Concept001.swift
@@ -23,6 +23,16 @@ struct Concept001: View {
         }
         .frame(width: circleWindowSize, height: circleWindowSize)
         .glassBackgroundEffect(in: .capsule, displayMode: .always)
+
+        .ornament(attachmentAnchor: .scene(.trailing), contentAlignment: .leading, ornament: {
+            VStack(spacing: 6) {
+                Text("The emoji here are intended to stand in for profile photos for each person.")
+                Text("Hover over each circle to see a full color version.")
+            }
+            .frame(width: 220)
+            .padding()
+            .glassBackgroundEffect()
+        })
     }
 }
 
